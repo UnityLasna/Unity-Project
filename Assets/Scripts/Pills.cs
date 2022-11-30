262294efd2eace925a93cadad8ae30a0e56bebdd
VeikkaSa <98524201+VeikkaSa@users.noreply.github.com>
2022-11-30 17:04:32 +0200

    soundit

--- Assets/Scripts/Pills.cs
+++ Assets/Scripts/Pills.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Pills : MonoBehaviour
 {
-   public float points = 0;
+    public float points = 0;
     public float pills;
-   private void OnTriggerEnter2D(Collider2D collision)
-   {
-    if (collision.name.Equals("Player")) {
-        // päivitetään scores 
-        GameObject.Find("Main Camera").GetComponent<scores>().points += 1f;
-            GameObject.Find("Main Camera").GetComponent<scores>().pills += 1f;
-        Destroy(this.gameObject);
+
+    [Header("SFX")]
+    [SerializeField] private AudioClip pillSound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Equals("Player")) {
+            // päivitetään scores 
+            SoundManager.instance.PlaySound(pillSound);
+            GameObject.Find("Main Camera").GetComponent<scores>().points += 1f;
+                GameObject.Find("Main Camera").GetComponent<scores>().pills += 1f;
+            Destroy(this.gameObject);
+        }
     }
-   }
 }
